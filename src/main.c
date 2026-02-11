40696b1ad383fc9cb6e5482fa5b2128dee21425d
Akash K N <akashkncse@gmail.com>
2026-02-11 22:46:46 +0530

    feat(dune-shell): implement shell loop, read line, split line

--- src/main.c
+++ src/main.c
@@ -1,10 +1,100 @@
 #include "main.h"
 
+char **dsh_split_line(char *line)
+{
+    int bufsize = DSH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+
+    if (!tokens)
+    {
+        fprintf(stderr, "dsh: allocation error!");
+        return EXIT_FAILURE;
+    }
+
+    token = strtok(line, DELIM);
+    while (token != NULL)
+    {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize)
+        {
+            bufsize += DSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize);
+            if (!tokens)
+            {
+                fprintf(stderr, "dsh: allocation error!");
+                return EXIT_FAILURE;
+            }
+        }
+        token = strtok(NULL, DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+char *dsh_read_line()
+{
+    int buf_size = DSH_RL_BUFSIZE;
+    char c;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * buf_size);
+
+    if (!buffer)
+    {
+        fprintf(stderr, "dsh: allocation error!");
+        return EXIT_FAILURE;
+    }
+
+    while (1)
+    {
+        c = getchar();
+
+        if (c == EOF || c == '\n')
+        {
+            buffer[position] = '\0';
+            return buffer;
+        }
+        else
+        {
+            buffer[position] = c;
+        }
+        position++;
+
+        if (position > buf_size)
+        {
+            buf_size += DSH_RL_BUFSIZE;
+            buffer = realloc(buffer, buf_size);
+            if (!buffer)
+            {
+                fprintf(stderr, "dsh: allocation error!");
+                return EXIT_FAILURE;
+            }
+        }
+    }
+}
+
+void dsh_loop()
+{
+    char *line;
+    char **args;
+    int status;
+
+    do
+    {
+        printf(">> ");
+        line = dsh_read_line();
+        args = dsh_split_line(line);
+        status = dsh_execute(args);
+        free(line);
+        free(args);
+    } while (status);
+}
+
 int main(int ac, char **av)
 {
-    int status;
-    if (fork() == 0)
-        execvp(av[1], av + 1);
-    wait(&status);
+    dsh_loop();
+
     return EXIT_SUCCESS;
 }
--- src/main.h
+++ src/main.h
@@ -4,4 +4,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+#define DSH_RL_BUFSIZE 1024
+#define DSH_TOK_BUFSIZE 64
+#define DELIM " \n\t\r\a"
 #endif
